adf29ff9c52af9b8f0f342d4ff570e5731d27069
Jaspreet Singh <jass.waraich@icloud.com>
2022-08-24 00:16:44 -0400

    add pages

---
+++ frontend/components/ToDoList.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="to-do-container">
+    <div class="create-container">
+      <input v-model="titleOfNewToDo" type="text">
+      <button @click="create">
+        Add
+      </button>
+    </div>
+    <div class="to-do-list-container">
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      titleOfNewToDo: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.to-do-container {
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  display: block;
+
+  .create-container {
+    width: 100%;
+    height: 50px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+
+    input {
+      height: 50px;
+      width: 250px;
+      border: none;
+      padding-left: 10px;
+      font-family: "Quicksand", "Source Sans Pro", -apple-system,
+        BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+        sans-serif;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+      }
+    }
+
+    button {
+      height: 45px;
+      float: right;
+      background: none;
+      width: auto;
+      margin: 0;
+      border: none;
+      cursor: pointer;
+      text-align: right;
+      padding-right: 20px;
+      font-size: 18px;
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+.to-do-list-container {
+  width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  display: block;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+}
+</style>
