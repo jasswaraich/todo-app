752afca3acbbda43161ea051e3dc19869434ef9a
Jaspreet Singh <jass.waraich@icloud.com>
2022-08-23 23:23:46 -0400

    update nuxtjs config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -47,5 +47,20 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
+
   }
 }
