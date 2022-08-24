cbf989a3ed719861def9239d565179e01da1e668
Jaspreet Singh <jass.waraich@icloud.com>
2022-08-24 00:53:49 -0400

    add login service

---
+++ frontend/assets/service/LoginService.js
@@ -0,0 +1,21 @@
+export default class LoginService {
+    constructor ($axios) {
+      this.axios = $axios
+    }
+  
+    performLogin (username, password) {
+      const data = new FormData()
+      data.append('username', username)
+      data.append('password', password)
+      return this.axios({
+        method: 'post',
+        url: 'login',
+        data
+      }).then(() => {
+        return true
+      }).catch(() => {
+        return false
+      })
+    }
+  }
+  
